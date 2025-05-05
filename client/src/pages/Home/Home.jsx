52bcda6d48ed7b60d558983b438d1ce290c4b8d9
nourist <hodinhvys@gmail.com>
2025-05-05 13:11:34 +0700

    design home page #3

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,17 +2,46 @@ import { motion } from 'framer-motion';
 import { Link } from 'react-router';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useTranslation } from 'react-i18next';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { MemoryStick, Clock9, Star, CircleCheck } from 'lucide-react';
+import { Button } from '~/components/ui/button';
+import Countdown from 'react-countdown';
 
 import useAuthStore from '~/stores/authStore';
 import UserAvatar from '~/components/UserAvatar';
 import routesConfig from '~/config/routes';
 import Trophy from '~/assets/icons/trophy.svg';
+import { getProblems } from '~/services/problem';
+import Contest from '~/assets/images/1stcontest.png';
+import { getContest } from '~/services/contest';
+import statusColors from '~/config/statusColor';
+import { getSubmissions } from '~/services/submission';
 
 const Home = () => {
 	const { user } = useAuthStore();
 	const { t } = useTranslation('home');
+	const [newestProblem, setNewestProblem] = useState();
+	const [currentContest, setCurrentContest] = useState();
+	const [statistic, setStatistic] = useState({});
 
-	console.log(user);
+	useEffect(() => {
+		getProblems({ size: 1, sortBy: 'createdAt', order: -1 })
+			.then((res) => {
+				setNewestProblem(res.data?.[0]);
+			})
+			.catch((err) => toast.error(err.response.data.msg));
+		if (user?.joiningContest)
+			getContest(user?.joiningContest)
+				.then((res) => {
+					setCurrentContest(res.data);
+				})
+				.catch((err) => toast.error(err.response.data.msg));
+		if (user)
+			getSubmissions({ author: user?.name })
+				.then((res) => setStatistic(res.stat.status))
+				.catch((err) => toast.error(err.response.data.msg));
+	}, [user]);
 
 	return (
 		<div className="flex-1">
@@ -81,20 +110,95 @@ const Home = () => {
 						initial={{ opacity: 0, y: 20 }}
 						animate={{ opacity: 1, y: 0 }}
 						transition={{ duration: 0.5 }}
-						className="col-span-4 row-start-3 rounded-lg dark:bg-zinc-800 dark:shadow-[0px_0px_7px_rgba(0,0,0,0.2)] bg-white shadow-[0px_0px_7px_rgba(0,70,155,0.2)] lg:col-span-2 lg:row-span-2"
-					></motion.div>
+						className="col-span-4 row-start-3 py-8 px-12 rounded-lg flex flex-col dark:bg-zinc-800 dark:shadow-[0px_0px_7px_rgba(0,0,0,0.2)] bg-white shadow-[0px_0px_7px_rgba(0,70,155,0.2)] lg:col-span-2 lg:row-span-2"
+					>
+						<div className="flex">
+							<h2 className="text-2xl dark:text-white capitalize">{t('newest-problem')}</h2>
+							<Link to={routesConfig.problems} className="ml-auto text-blue-600 hover:underline capitalize text-sm">
+								{t('view-more')}
+							</Link>
+						</div>
+						{!newestProblem ? (
+							<div className="flex flex-1 text-sm items-center capitalize justify-center flex-col dark:text-gray-300 gap-2">
+								<h2 className="text-2xl dark:text-white">{'(╯°□°）╯︵ ┻━┻'}</h2>
+								{t('no-problem-found')}!
+							</div>
+						) : (
+							<div className="flex my-auto gap-4">
+								<div
+									data-difficulty={newestProblem.difficulty}
+									className="size-16 capitalize flex items-center justify-center text-2xl font-bold data-[difficulty=easy]:bg-green-500 data-[difficulty=medium]:bg-yellow-400 data-[difficulty=hard]:bg-red-500 data-[difficulty=easy]:border-green-500 data-[difficulty=medium]:border-yellow-400 data-[difficulty=hard]:border-red-500 data-[difficulty=easy]:text-green-500 data-[difficulty=medium]:text-yellow-400 data-[difficulty=hard]:text-red-500 border-2 rounded-lg !bg-opacity-30 "
+								>
+									{newestProblem.difficulty[0]}
+								</div>
+								<div className="text-xl dark:text-white flex flex-col justify-around">
+									{newestProblem.name}
+									<div className="flex text-sm dark:text-gray-400 gap-1">
+										<Star className="size-4" strokeWidth={1}></Star>
+										{newestProblem.point}
+										<CircleCheck className="size-4 ml-2" strokeWidth={1}></CircleCheck>
+										{newestProblem.noOfSubm === 0 ? 0 : Math.round((newestProblem.noOfSuccess / newestProblem.noOfSubm) * 100)}%
+										<MemoryStick className="size-4 ml-2" strokeWidth={1}></MemoryStick>
+										{newestProblem.memoryLimit}MB
+										<Clock9 className="size-4 ml-2" strokeWidth={1}></Clock9>
+										{newestProblem.timeLimit}s
+									</div>
+								</div>
+								<Button asChild className="ml-auto my-auto w-32 h-8 !bg-blue-500 rounded-sm !text-white">
+									<Link to={routesConfig.problem.replace(':id', newestProblem.id)}>
+										<div className="text-xl">{'>'}</div>
+										{t('try-now')}
+									</Link>
+								</Button>
+							</div>
+						)}
+					</motion.div>
 					<motion.div
 						initial={{ opacity: 0, y: 20 }}
 						animate={{ opacity: 1, y: 0 }}
 						transition={{ duration: 0.5 }}
-						className="col-span-2 row-start-4 rounded-lg dark:bg-zinc-800 dark:shadow-[0px_0px_7px_rgba(0,0,0,0.2)] bg-white shadow-[0px_0px_7px_rgba(0,70,155,0.2)] lg:row-span-2 lg:col-start-3 lg:row-start-3 lg:col-span-1"
-					></motion.div>
+						className="col-span-2 row-start-4 py-6 px-12 rounded-lg flex flex-col dark:bg-zinc-800 dark:shadow-[0px_0px_7px_rgba(0,0,0,0.2)] bg-white shadow-[0px_0px_7px_rgba(0,70,155,0.2)] lg:row-span-2 lg:col-start-3 lg:row-start-3 lg:col-span-1"
+					>
+						<h2 className="text-2xl dark:text-white capitalize">{t('current-contest')}</h2>
+						<div className="flex flex-1 items-center justify-center gap-4">
+							{!user.joiningContest ? (
+								<h3 className="dark:text-gray-400">{t('no-contest-join-msg')}</h3>
+							) : (
+								<>
+									<img src={Contest} alt="" />
+									<div className="flex-col flex">
+										<Link
+											to={routesConfig.contest.replace(':id', user.joiningContest)}
+											className="hover:underline hover:!text-yellow-400 text-lg capitalize dark:text-white"
+										>
+											{currentContest?.title}
+										</Link>
+										<Countdown className="dark:text-slate-400" date={new Date(currentContest?.endTime) || Date.now()}></Countdown>
+									</div>
+								</>
+							)}
+						</div>
+					</motion.div>
 					<motion.div
 						initial={{ opacity: 0, y: 20 }}
 						animate={{ opacity: 1, y: 0 }}
 						transition={{ duration: 0.5 }}
-						className="col-span-2 col-start-3 row-start-4 rounded-lg dark:bg-zinc-800 dark:shadow-[0px_0px_7px_rgba(0,0,0,0.2)] bg-white shadow-[0px_0px_7px_rgba(0,70,155,0.2)] lg:row-span-2 lg:col-start-4 lg:row-start-3 lg:col-span-1"
-					></motion.div>
+						className="col-span-2 col-start-3 row-start-4 py-6 px-12 rounded-lg dark:bg-zinc-800 dark:shadow-[0px_0px_7px_rgba(0,0,0,0.2)] bg-white shadow-[0px_0px_7px_rgba(0,70,155,0.2)] lg:row-span-2 lg:col-start-4 lg:row-start-3 lg:col-span-1"
+					>
+						<h2 className="text-2xl dark:text-white capitalize mb-2">{t('your-submissions')}</h2>
+
+						<div className="grid grid-cols-2 grid-rows-4 gap-2">
+							{[
+								statusColors.ac, // AC - green-500
+								statusColors.wa, // WA - red-500
+								statusColors.tle, // TLE - yellow-500
+								statusColors.mle, // MLE - blue-500
+								statusColors.rte, // RTE - orange-500
+								statusColors.ce, // CE - rose-500
+								statusColors.ie, // IE - purple-600
+							].map((item, index) => statistic[index])}
+						</div>
+					</motion.div>
 				</div>
 			</div>
 		</div>
