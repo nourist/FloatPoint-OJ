57f49caa8fd79371c04c8ce7ebfaad0c92182454
nourist <hodinhvys@gmail.com>
2025-05-17 20:43:12 +0700

    feat: design footer

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -2,13 +2,17 @@ import PropTypes from 'prop-types';
 
 import Sidebar from '~/components/Sidebar';
 import Header from '~/components/Header';
+import Footer from '~/components/Footer';
 
 const DefaultLayout = ({ children }) => {
 	return (
 		<>
 			<Sidebar />
 			<Header />
-			<div className="px-6 pt-[102px] lg:pl-[274px]">{children}</div>
+			<div className="px-6 pt-[102px] lg:pl-[274px]">
+				{children}
+				<Footer />
+			</div>
 		</>
 	);
 };
