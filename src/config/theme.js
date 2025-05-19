bb533d7980d983ed78459969e9581a4b5501ef9c
nourist <hodinhvys@gmail.com>
2025-05-19 21:55:39 +0700

    style: custom theme for darkmode

--- src/config/theme.js
+++ src/config/theme.js
@@ -9,6 +9,57 @@ const theme = {
 			},
 		},
 	},
+	iconButton: {
+		styles: {
+			base: {
+				color: '!text-base-content',
+			},
+		},
+	},
+	menu: {
+		styles: {
+			base: {
+				menu: {
+					bg: 'bg-base-100',
+					border: 'border border-base-300',
+				},
+				item: {
+					initial: {
+						bg: 'hover:bg-base-300',
+						color: 'dark:text-blue-gray-300 hover:!text-base-content',
+					},
+				},
+			},
+		},
+	},
+	input: {
+		styles: {
+			base: {
+				input: {
+					placeholder: 'placeholder-base-content/60',
+					color: 'text-base-content',
+				},
+			},
+			variants: {
+				outlined: {
+					base: {
+						input: {
+							borderColor: '!border-blue-gray-200 dark:!border-blue-gray-700',
+						},
+					},
+				},
+			},
+		},
+	},
+	checkbox: {
+		styles: {
+			base: {
+				input: {
+					borderColor: 'border-blue-gray-200 dark:border-blue-gray-700',
+				},
+			},
+		},
+	},
 };
 
 export default theme;
