7e6864d207f2e5725e697c411a0be7a57369668c
Nourist <hodinhvys@gmail.com>
2025-08-03 20:54:28 +0700

    feat(server): use migration

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,50 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { addTransactionalDataSource } from 'typeorm-transactional';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			validationSchema: Joi.object({
+				PORT: Joi.number().default(4000),
+				CLIENT_URL: Joi.string().default('http://localhost:3000'),
+				DB_HOST: Joi.string().default('localhost'),
+				DB_PORT: Joi.number().default(5432),
+				DB_USERNAME: Joi.string().default('postgres'),
+				DB_PASSWORD: Joi.string().default(''),
+				DB_NAME: Joi.string().default('postgres'),
+			}),
+		}),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) => ({
+				type: 'postgres',
+				host: configService.get<string>('DB_HOST'),
+				port: configService.get<number>('DB_PORT'),
+				username: configService.get<string>('DB_USERNAME'),
+				password: configService.get<string>('DB_PASSWORD'),
+				database: configService.get<string>('DB_NAME'),
+				entities: [__dirname + '/entities/*.entity{.ts,.js}'],
+				synchronize: false,
+			}),
+			dataSourceFactory: (options) => {
+				if (!options) {
+					throw new Error('Invalid options passed');
+				}
+
+				const ds = new DataSource(options);
+				return ds.initialize().then(addTransactionalDataSource);
+			},
+		}),
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,57 @@
 import { NestFactory } from '@nestjs/core';
+import { initializeTransactionalContext } from 'typeorm-transactional';
+import { ValidationPipe } from '@nestjs/common';
+import { ValidationError } from 'class-validator';
+import { UnprocessableEntityException } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	await app.listen(process.env.PORT ?? 3000);
+	initializeTransactionalContext();
+
+	const app = await NestFactory.create(AppModule, {
+		logger: ['error', 'warn', 'log', 'verbose', 'debug'],
+	});
+	const configService = app.get(ConfigService);
+
+	app.enableCors({
+		origin: configService.get<string>('CLIENT_URL'),
+		methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+		credentials: true,
+	});
+
+	app.use(cookieParser());
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			transform: true,
+			transformOptions: {
+				enableImplicitConversion: true,
+			},
+			exceptionFactory: (validationErrors: ValidationError[] = []) => {
+				return new UnprocessableEntityException({
+					statusCode: 422,
+					error: 'Unprocessable Entity',
+					message: 'Validation failed',
+					fieldErrors: validationErrors.reduce(
+						(acc, error) => ({
+							...acc,
+							[error.property]: Object.values(error?.constraints ?? {}).reduce((acc, cur) => cur, ''),
+						}),
+						{},
+					),
+				});
+			},
+		}),
+	);
+
+	app.useGlobalFilters(new AllExceptionsFilter());
+
+	await app.listen(configService.get<number>('PORT')!);
 }
 
 void bootstrap();
